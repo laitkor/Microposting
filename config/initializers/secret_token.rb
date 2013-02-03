ef87c2fd85fda43acaa33920d43135b192f9e048
Akash Agrawal <akashspeaking@gmail.com>
2013-02-03 12:46:32 +0530

    Crash Rec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '01f80e96a1f8f10ce0d8f19374bf1ddb9a8bea2c7b4c75ff97889ada0d20b9bd1f2bb52ca61a4d4329c69fe72dd46864e162e2b3d6820f5d7a435f365ee96a6d'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Akash Agrawal	"
+    email    "akashspeaking@outlook.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
